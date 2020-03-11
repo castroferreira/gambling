615bd893c3dd23989008c7f0f7035bbc12add14d
Andreia Castro Ferreira <castroferreira@ua.pt>
2020-03-11 23:24:32 +0000

    sidebar

--- js/toggle.js
+++ js/toggle.js
@@ -1,39 +1,18 @@
-var theToggle = document.getElementById('toggle');
+$(document).ready(function() {
 
-// hasClass
-function hasClass(elem, className) {
-    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-}
-// addClass
-function addClass(elem, className) {
-    if (!hasClass(elem, className)) {
-        elem.className += ' ' + className;
+    function toggleSidebar() {
+        $(".button").toggleClass("active");
+        $("main").toggleClass("move-to-left");
+        $(".sidebar-item").toggleClass("active");
     }
-}
-// removeClass
-function removeClass(elem, className) {
-    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
-    if (hasClass(elem, className)) {
-        while (newClass.indexOf(' ' + className + ' ') >= 0) {
-            newClass = newClass.replace(' ' + className + ' ', ' ');
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    }
-}
-// toggleClass
-function toggleClass(elem, className) {
-    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, " ") + ' ';
-    if (hasClass(elem, className)) {
-        while (newClass.indexOf(" " + className + " ") >= 0) {
-            newClass = newClass.replace(" " + className + " ", " ");
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    } else {
-        elem.className += ' ' + className;
-    }
-}
 
-theToggle.onclick = function() {
-    toggleClass(this, 'on');
-    return false;
-}
+    $(".button").on("click tap", function() {
+        toggleSidebar();
+    });
+
+    $(document).keyup(function(e) {
+        if (e.keyCode === 27) {
+            toggleSidebar();
+        }
+    });
+});
